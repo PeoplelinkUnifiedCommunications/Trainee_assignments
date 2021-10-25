4f8c4ba0729f030f216dd9500ddd24615794db5b
Vamsi Krishna Yadla <vamsikrishnayadla@Vamsis-MacBook-Air.local>
2021-10-25 13:13:54 +0530

    BCS-I171 Assignment 4

--- first/src/components/header/header.js
+++ first/src/components/header/header.js
@@ -1,19 +1,16 @@
-import React from 'react'
-import "./headerstyles.css"
+import React from "react";
+import "./headerstyles.css";
 
 function header() {
-    return (
-
-        <div className="header"> 
-        <p><i class="fa fa-cloud-download fa-2x" style={{color:'white'}}></i>
-         <span className="plus-icon">+</span>
-        </p>
-            <p>cloud inventory</p>
-           
-
-        </div>
-        
-    )
+  return (
+    <div className="header">
+      <p>
+        <i class="fa fa-cloud-download fa-2x" style={{ color: "white" }}></i>
+        <span className="plus-icon">+</span>
+      </p>
+      <p>cloud inventory</p>
+    </div>
+  );
 }
 
-export default header
+export default header;
