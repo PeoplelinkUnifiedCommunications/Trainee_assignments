1a10e76a9da6d40355c19553ae7f02e16ff6852b
saikrishna-73 <saikrishna@peoplelinkvc.com>
2021-10-26 14:32:56 +0530

    UI Assignment 3Projects

---
+++ project2/index.js
@@ -0,0 +1,8 @@
+let move = document.getElementById("moving_btn") ;
+let change = document.getElementById("Tbtn") ;
+
+move.addEventListener('click' , function (){
+    move.classList.toggle('moveright') ;
+    
+   
+})
