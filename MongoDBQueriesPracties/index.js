ee9e7b4862b75c924e12454e1009fc7974eee97b
Harish9245 <harish.s@peoplelinkvc.com>
2022-01-13 18:41:48 +0530

    mongoDB queries practice

---
+++ MongoDBQueriesPracties/index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const app = express();
+app.use(express.json());
+
+const db = async () => {
+	try {
+		await mongoose.connect("mongodb://localhost:27017/practiceData");
+		console.log("db conencted.....");
+		app.listen(3007, () => {
+			console.log("Server running at http://localhost:3007");
+		});
+	} catch (error) {
+		console.log(error.massage);
+	}
+};
+
+db();
+
+const moviesSchema = new Schema({
+	title: String,
+	writer: String,
+	year: Number,
+	actors: [String],
+	franchise: String,
+});
+
+const moviesSchemaModel = mongoose.model("movies", moviesSchema);
+
+app.post("/addMovies/", async (req, res) => {
+	const addMoviesData = await moviesSchemaModel.insertMany(req.body);
+	res.send("added successfully");
+});
+
+app.post("/addOneMovie/", async (req, res) => {
+	const addOneMOvie = await moviesSchemaModel.create(req.body);
+	res.send("add one movie is succussfully");
+});
+
+app.get("/getMovieData/", async (req, res) => {
+	const getData = await moviesSchemaModel.find();
+	res.send(getData);
+});
+
+app.get("/getMovieByWriter/:writer", async (req, res) => {
+	const { writer } = req.params;
+	const getMovieByWriter = await moviesSchemaModel.find({ writer: writer });
+	res.send(getMovieByWriter);
+});
+
+app.get("/getMovieYear/", async (req, res) => {
+	const { year1, year2 } = req.query;
+	const getMovieByYear = await moviesSchemaModel.find({
+		year: { $gte: year2, $lte: year1 },
+	});
+	res.send(getMovieByYear);
+});
+
+app.get("/getMovieByActorAndWriter/", async (req, res) => {
+	const { year, writer } = req.query;
+	const getMovieByActorAndWriter = await moviesSchemaModel.find({
+		writer: writer,
+		year: year,
+	});
+	res.send(getMovieByActorAndWriter);
+});
+
+module.exports = app;
