383b8005bd929b14395bb00d4178df08eebc5675
karisinapuchinnababu1 <chinnababu@peoplelinkvc.com>
2022-01-22 13:11:46 +0530

    BCS-I1525 Github user search

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Search from './component/Search field';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Search/>
+    </div>
+  );
+}
+
+export default App;
