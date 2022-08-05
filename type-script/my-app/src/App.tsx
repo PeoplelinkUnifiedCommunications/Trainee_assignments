2fde1d8cf559939e936cb09602630269266da393
santhoshi <santhoshi@peoplelink.com>
2022-08-05 19:32:49 +0530

    #Ass-I45 run the  type script

---
+++ type-script/my-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AboutTypeScript from './container/AboutTypeScript';
+
+function App() {
+  return (
+    <div className="App">
+      <AboutTypeScript />
+    </div>
+  );
+}
+
+export default App;
---
+++ type-script/my-app/src/container/AboutTypeScript/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutTypeScript = () => {
+  return (
+    <div>
+        <input  type="text"/>
+        <button>submit</button>
+    </div>
+  )
+}
+
+export default AboutTypeScript
